fe61b6913bc5f58252d9e5afdad18facbc4f0d5b
hanahpu <57234681+hanahpu@users.noreply.github.com>
2019-10-31 19:07:53 -0600

    Update variables.tf
    Removing key

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "instance_type" {
 
 variable "ssh_key" {
 	description = "Public Key"
-	default = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBbZDrvUhOr/4HCqGW2GNStQcqiOrM1qSE1NgD2/TZauB5cVxRIFJ4tJ+1EQFTskg5isfv+u5vIAuZqtXVWAu+vESe6snwPEjTtYc3uJ3qMvy1FwKs0az2YZqeJnKRegrLYvYXdAOzi01PufXq73c2ShP+lPzukL4MuTEb4/5VafWFhPC4MrSWTLSJzj36yNozB7Cayr4/Odwi2ZqRDLZnsN+bsQX+1K594krQlMyC6LxiU3pBWDu5dNNG3rF7Uv1GuphPnzG2Sz5YN2K+4YyvS9e+VUGsW/dWb43+eG3ispFdAY3OpsrxNW6G2HLnYMn4Nwsqlg7pky9007Qy9IPSd rsa-key-20190619"
+	default = "ssh-rsa Lorem Ipsum"
 }
 
 variable "user" {
